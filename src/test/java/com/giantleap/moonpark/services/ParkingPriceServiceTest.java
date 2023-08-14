bd01c21338ec3ffce76e406140ac75e011050af2
Arturo Batistute <arturob@gritsolutions.pt>
2023-08-14 21:45:48 +0100

    100% app coverage

---
+++ src/test/java/com/giantleap/moonpark/services/ParkingPriceServiceTest.java
@@ -0,0 +1,78 @@
+package com.giantleap.moonpark.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.giantleap.moonpark.exceptions.MoonParkException;
+import com.giantleap.moonpark.model.PriceDetailsRecord;
+import com.giantleap.moonpark.model.enums.ParkingZoneEnum;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ParkingPriceServiceTest {
+
+    @Spy
+    private ParkingPriceStrategy parkingPriceStrategy;
+
+    @Spy
+    private ParkingPriceContext parkingPriceContext;
+
+    @InjectMocks
+    private ParkingPriceService parkingPriceService;
+
+    @Test
+    void calculatePrice_withStartDateTimeBeforeEndStartDate_shouldThrowException() {
+
+        String startDateTime = "2023-08-15 14:00:00";
+        String endDateTime = "2023-08-13 14:01:00";
+
+        assertThrows(MoonParkException.class, () -> parkingPriceService.calculatePrice(ParkingZoneEnum.M1.name(), startDateTime, endDateTime));
+    }
+
+    @Test
+    void calculatePrice_withNonexistentParkingZone_shouldThrowException() {
+
+        String startDateTime = "2023-08-13 14:00:00";
+        String endDateTime = "2023-08-13 14:01:00";
+
+        assertThrows(MoonParkException.class, () -> parkingPriceService.calculatePrice("M0", startDateTime, endDateTime));
+    }
+
+    @Test
+    void calculatePrice_withM1Area_shouldReturnAmountToPayInsidePriceDetails() {
+
+        String startDateTime = "2023-08-13 14:00:00";
+        String endDateTime = "2023-08-13 14:01:00";
+
+        PriceDetailsRecord priceDetails = parkingPriceService.calculatePrice(ParkingZoneEnum.M1.name(), startDateTime, endDateTime);
+
+        assertEquals("1.0", priceDetails.price());
+    }
+
+    @Test
+    void calculatePrice_withM2Area_shouldReturnAmountToPayInsidePriceDetails() {
+
+        String startDateTime = "2023-08-14 14:00:00";
+        String endDateTime = "2023-08-14 14:01:00";
+
+        PriceDetailsRecord priceDetails = parkingPriceService.calculatePrice(ParkingZoneEnum.M2.name(), startDateTime, endDateTime);
+
+        assertEquals("1.67", priceDetails.price());
+    }
+
+    @Test
+    void calculatePrice_withM3Area_shouldReturnAmountToPayInsidePriceDetails() {
+
+        String startDateTime = "2023-08-14 08:00:00";
+        String endDateTime = "2023-08-14 09:01:00";
+
+        PriceDetailsRecord priceDetails = parkingPriceService.calculatePrice(ParkingZoneEnum.M3.name(), startDateTime, endDateTime);
+
+        assertEquals("2.0", priceDetails.price());
+    }
+
+
+}
