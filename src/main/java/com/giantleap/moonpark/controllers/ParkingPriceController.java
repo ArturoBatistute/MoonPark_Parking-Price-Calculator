0b55de7b2d649e34c0a47c77efd2eeef94e70243
Arturo Batistute <arturob@gritsolutions.pt>
2023-08-14 22:13:13 +0100

    Implement controller advice to handle exceptions

--- src/main/java/com/giantleap/moonpark/controllers/ParkingPriceController.java
+++ src/main/java/com/giantleap/moonpark/controllers/ParkingPriceController.java
@@ -5,6 +5,7 @@ import com.giantleap.moonpark.services.ParkingPriceService;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,7 @@ public class ParkingPriceController {
     @Autowired
     private ParkingPriceService parkingPriceService;
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "Success"),
         @ApiResponse(responseCode = "400", description = "Bad Request"),
